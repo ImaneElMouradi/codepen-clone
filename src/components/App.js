3fae954d06f692d865b7573f046a40227479a791
Phedri <asphedri@gmail.com>
2020-10-16 08:12:19 +0200

    feat(bottom-panel): add result iframe

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Editor from './Editor';
 
 function App() {
   const [html, setHtml] = useState('');
   const [css, setCss] = useState('');
   const [js, setJs] = useState('');
+  const [srcDoc, setSrcDoc] = useState('');
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(
+        `<html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>`
+      );
+    }, 250);
+
+    return () => clearTimeout(timeout);
+  }, [css, html, js]);
 
   return (
     <>
@@ -21,18 +36,24 @@ function App() {
           value={css}
           onChange={setCss}
         />
-        <Editor language="js" displayName="JS" value={js} onChange={setJs} />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
       </div>
 
-      <div className="panel"></div>
-
-      <iframe
-        title="output"
-        sandbox="allow-scripts"
-        frameBorder="0"
-        width="100%"
-        height="100%"
-      />
+      <div className="panel">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
     </>
   );
 }
