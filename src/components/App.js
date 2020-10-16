6157ccf3d3492c5f2f839a0125169691b9693069
Phedri <asphedri@gmail.com>
2020-10-16 07:58:56 +0200

    feat(top-panel): add 3 editors

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Editor from './Editor';
 
 function App() {
+  const [html, setHtml] = useState('');
+  const [css, setCss] = useState('');
+  const [js, setJs] = useState('');
+
   return (
-    <div className="App">
-      <p>Start codepen clone project.</p>
-    </div>
+    <>
+      <div className="panel top-panel">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor language="js" displayName="JS" value={js} onChange={setJs} />
+      </div>
+
+      <div className="panel"></div>
+
+      <iframe
+        title="output"
+        sandbox="allow-scripts"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+      />
+    </>
   );
 }
 
